40565ca87b1f4db0d7e6f484fce41709008e4864
Banx87 <joni.lindholm@gmail.com>
2019-03-03 21:25:39 +0200

    changing response

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -60,9 +60,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send(db.users);
-    //res.send('this is working');
+app.get('/', (req, res) => { //res.send(db.users);
+    res.send('this is working');
 });
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
