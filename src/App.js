b73cefae3d5bacd37e88a4e3cdab99291f24b06a
Banx87 <joni.lindholm@gmail.com>
2019-03-04 17:49:20 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ class App extends Component {
                 .then(response => response.json())
                 .then(response => {
                     if(response) {
-                        fetch('https://git.heroku.com/desolate-inlet-25995.git/:3000/image', {
+                        fetch('https://git.heroku.com/desolate-inlet-25995.git/image', {
                             method: 'put',
                             headers: {'Content-Type': 'application/json'},
                             body: JSON.stringify({
