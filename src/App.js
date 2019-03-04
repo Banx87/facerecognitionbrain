a09c5d43e159db401ff1e56bc22d790b057cbfd2
Banx87 <joni.lindholm@gmail.com>
2019-03-04 15:24:49 +0200

    smart brain api
    Testing

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends Component {
 
     onPictureSubmit = () => {
         this.setState({imageUrl: this.state.input});
-               fetch('http://localhost:3000/imageurl', {
+               fetch('https://thawing-shore-93064.herokuapp.com/imageurl', {
                 method: 'post',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({
@@ -96,7 +96,7 @@ class App extends Component {
                 .then(response => response.json())
                 .then(response => {
                     if(response) {
-                        fetch('http://localhost:3000/image', {
+                        fetch('https://thawing-shore-93064.herokuapp.com/:3000/image', {
                             method: 'put',
                             headers: {'Content-Type': 'application/json'},
                             body: JSON.stringify({
